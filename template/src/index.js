16f4cbc43d0cf7d5039676b08f354836b2468e70
liuyao <reactjser@163.com>
2020-07-22 16:00:24 +0800

    国际化

--- template/src/index.js
+++ template/src/index.js
@@ -1,6 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ConfigProvider } from 'antd';
+import zhCN from 'antd/es/locale/zh_CN';
+import moment from 'moment';
+import 'moment/locale/zh-cn';
 import './assets/styles/index.scss';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+moment.locale('zh-cn');
+
+ReactDOM.render(
+  <ConfigProvider locale={zhCN}>
+    <App />
+  </ConfigProvider>,
+  document.getElementById('root'),
+);
